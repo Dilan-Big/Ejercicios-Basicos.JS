a99f714bcd7a290c341039ff47157dee8cbbb49b
Dilan Gonzalez <dilanbigdata@gmail.com>
2026-04-06 19:33:24 -0500

    Array

---
+++ Array/script.js
@@ -0,0 +1,12 @@
+//
+let productos = [];
+
+let pro1 = prompt("Ingrese un dato1: ");
+let pro2 = prompt("Ingrese un dato2: ");
+let pro3 = prompt("Ingrese un dato3: ");
+
+productos.push(pro1);
+productos.push(pro2);
+productos.push(pro3);
+
+console.log(productos);
