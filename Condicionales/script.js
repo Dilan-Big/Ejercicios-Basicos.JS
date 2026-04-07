b03f2d6607bd34d899b3581100935913504c777e
Dilan Gonzalez <dilanbigdata@gmail.com>
2026-04-06 20:48:52 -0500

    ejer_2

--- Condicionales/script.js
+++ Condicionales/script.js
@@ -1,9 +1,9 @@
 // 1 cuanto costo el produco 
-// 2 si el producto  cueta - 100 aumento del 10% por el envio
-// entr 100  a 200 descuento de 10% si cuenta mas de 200 25% de descuento 
+// 2 si el producto  cuesta - 100 aumento del 10% por el envio
+// entre 100 a 200 descuento del 10% si cuenta mas de 200 25% de descuento 
 
 let precio = Number(prompt("Ingrese el precio del producto: "));
-let total = 0;
+
 
 if (precio < 100) {
     total = precio + (precio * 0.10);
